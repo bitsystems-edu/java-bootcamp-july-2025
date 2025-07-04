eb58b71e10460c23011461a967164c500de82c11
bitsystems-edu <bitsystems.edu@gmail.com>
2025-07-04 19:16:17 +0530

    First Java Program

---
+++ core-java-bootcamp/src/DAY01/org/bitsystem/firstjavaprogram/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+package DAY01.org.bitsystem.firstjavaprogram;
+
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java !");
+    }
+}

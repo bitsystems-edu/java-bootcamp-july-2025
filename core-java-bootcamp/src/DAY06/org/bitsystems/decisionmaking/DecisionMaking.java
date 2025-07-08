ef810d4625d8a3edae854fc4fddbc4f34b8147cd
bitsystems-edu <bitsystems.edu@gmail.com>
2025-07-08 22:29:51 +0530

    Decision Making

---
+++ core-java-bootcamp/src/DAY06/org/bitsystems/decisionmaking/DecisionMaking.java
@@ -0,0 +1,19 @@
+package DAY06.org.bitsystems.decisionmaking;
+
+import java.util.Scanner;
+
+public class DecisionMaking {
+    public static void main(String[] args) {
+        // ask input from user
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please Enter number for validation: ");
+        int number = sc.nextInt();
+
+        if(number % 2 == 0) {
+            System.out.println("Given number " + number + " is Even !");
+        }else {
+            System.out.println("Given number " + number + " is Odd !");
+        }
+    }
+}

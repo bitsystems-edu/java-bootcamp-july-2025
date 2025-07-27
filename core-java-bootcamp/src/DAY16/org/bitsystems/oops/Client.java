bb072b5fa5c6937e17339761960748d1bc5ff012
bitsystems-edu <bitsystems.edu@gmail.com>
2025-07-27 21:34:50 +0530

    OOps-Part1

---
+++ core-java-bootcamp/src/DAY16/org/bitsystems/oops/Client.java
@@ -0,0 +1,27 @@
+package DAY16.org.bitsystems.oops;
+
+public class Client {
+    public static void main(String[] args) {
+
+        /*
+          int number  = 10;
+          <Datatype> <variable Name> = <value>
+
+         */
+        Student ravi = new Student();
+        ravi.setAge(26);
+        ravi.getAge();
+        System.out.println(ravi.getAge());
+//        ravi.id = 1;
+//        ravi.name = "ravi";
+//        ravi.standard = "B.Tech -  4th Year";
+//        ravi.age = 86;
+//        ravi.qulifyingExam(ravi.id, ravi.name, ravi.age);
+//
+//        Student neha = new Student();
+//        neha.id = 2;
+//        neha.name = " Neha";
+//        neha.age = 25;
+//        neha.qulifyingExam(neha.id, neha.name, neha.age);
+    }
+}

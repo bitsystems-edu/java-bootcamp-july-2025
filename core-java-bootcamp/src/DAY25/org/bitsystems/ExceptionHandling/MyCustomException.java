e0ed75788982b1d85b42811d6895747a6c703972
bitsystems-edu <bitsystems.edu@gmail.com>
2025-08-12 22:47:41 +0530

    exceptin handling

---
+++ core-java-bootcamp/src/DAY25/org/bitsystems/ExceptionHandling/MyCustomException.java
@@ -0,0 +1,7 @@
+package DAY25.org.bitsystems.ExceptionHandling;
+
+public class MyCustomException extends Exception {
+    public MyCustomException(String message) {
+        super(message);
+    }
+}
